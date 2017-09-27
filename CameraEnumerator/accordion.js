9b9c6ac2752a8d9151b30d1ccd93cdae664ca866
dangerman79 <dan@fuzzy-logic.co.uk>
2017-09-27 18:20:06 +0100

    accordion working

---
+++ CameraEnumerator/accordion.js
@@ -0,0 +1,31 @@
+function setupAccordion ()
+{
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+	  acc[i].onclick = function() {
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		if (panel.style.maxHeight){
+		  panel.style.maxHeight = null;
+		} else {
+		  panel.style.maxHeight = panel.scrollHeight + "px";
+		} 
+	  }
+	}
+}
+
+function addAccordionSegment(parentElement, label, id)
+{
+	currentHTML = parentElement.innerHTML
+	newHTML = '<button class="accordion">'+ label +'</button>'
+	newHTML = newHTML + '<div class="panel" id="' + id + '">'
+	newHTML = newHTML + '</div>'
+	parentElement.innerHTML = newHTML + currentHTML
+	
+	
+	
+	
+	//setupAccordion ()
+}
--- CameraEnumerator/enumerator.js
+++ CameraEnumerator/enumerator.js
@@ -1,15 +1,13 @@
 
 	function createVideoWindow (iparentElement, iid)
 	{
-		parentElement = iparentElement
-		id = iid
-		parentElement.innerHTML = "Hello"
-		/*	<div class='select'>
-				<label for='videoSource'>Video source: </label><select id='videoSource'+id+''></select>
-			</div>
-			<video autoplay='true' id='videoElement'+id+''/>
-		";*/
-		
+		parentElement = iparentElement;
+		id = iid;
+		webcamHTML = "<div class='select'>";
+		webcamHTML = webcamHTML + "<label for='videoSource'>Video source: </label><select id='videoSource"+id+"'></select>";
+		webcamHTML = webcamHTML + "</div>"
+		webcamHTML = webcamHTML + "<video autoplay='true' class='videoWindow' id='videoElement"+id+"'/>"
+		parentElement.innerHTML = webcamHTML
 		video = document.querySelector('#videoElement' + id);
 		videoSelect  = document.querySelector('select#videoSource' + id);
 	}
